f24c6ead6a2fcf555f45773786abfb0fadb95c2c
yasemin <yasemin.goekuguz@gmail.com>
2025-03-05 12:30:07 +0100

    ss

--- pages/2_Unterseite B.py
+++ pages/2_Unterseite B.py
@@ -24,15 +24,15 @@ if hgb > 0 and erythrozyten > 0 and geschlecht != "Wählen Sie aus":
         st.write("Für Männer liegt der normale MCH-Wert typischerweise zwischen 27.5 und 33 pg.")
         # Überprüfung, ob der MCH-Wert im normalen Bereich liegt
         if 27.5 <= mch <= 33:
-            st.write("Der MCH-Wert liegt im normalen Bereich für Männer.")
+            st.write("Der MCH-Wert liegt im Referenzbereich für Männer.")
         else:
-            st.write("Der MCH-Wert liegt nicht im normalen Bereich für Männer.")
+            st.write("Der MCH-Wert liegt nicht im Referenzbereich für Männer.")
     else:
         st.write("Für Frauen liegt der normale MCH-Wert typischerweise zwischen 26 und 32.5 pg.")
         # Überprüfung, ob der MCH-Wert im normalen Bereich liegt
         if 26 <= mch <= 32.5:
-            st.write("Der MCH-Wert liegt im normalen Bereich für Frauen.")
+            st.write("Der MCH-Wert liegt im Referenzbereich für Frauen.")
         else:
-            st.write("Der MCH-Wert liegt nicht im normalen Bereich für Frauen.")
+            st.write("Der MCH-Wert liegt nicht im Referenzbereich für Frauen.")
 else:
     st.write("Bitte geben Sie gültige Werte für Hämoglobin, Erythrozytenanzahl und Geschlecht ein.")
