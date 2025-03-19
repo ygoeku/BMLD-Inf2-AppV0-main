33fa6859e3e4a08a6cba2c02da32422e43d4e492
Elena <avkovele@students.zhaw.ch>
2025-03-19 10:24:45 +0100

    änderung

--- Start.py
+++ Start.py
@@ -1,5 +1,22 @@
 import streamlit as st
 import pandas as pd
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+# initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="App_Elena_Yasemin")  # switch drive 
+
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
+
+# load the data from the persistent storage into the session state
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
 
 st.title("Meine erste Streamlit App")
 
